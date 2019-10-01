fc9cd1f66917e241c47c013e9089cc0ea7cf2bce
kid009 <pongpoom1985@gmail.com>
2019-10-01 18:09:50 +0700

    change

---
+++ WK/WK/Base.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WK
+{
+    public class Base
+    {
+        string connect = ConfigurationManager.ConnectionStrings["SqlConnection"].ToString();
+
+        public SqlConnection GetConnection()
+        {
+            SqlConnection sqlconnection = null;
+
+            if (!connect.Equals(""))
+            {
+                sqlconnection = new SqlConnection(connect); //สร้างการ connection
+
+                if (sqlconnection != null && sqlconnection.State == ConnectionState.Closed)
+                {
+                    sqlconnection.Open();
+                }
+            }
+
+            return sqlconnection;
+        }//GetConnection()
+    }
+}
---
+++ WK/WK/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WK.Startup))]
+namespace WK
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
