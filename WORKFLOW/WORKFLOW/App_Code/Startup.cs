193e5468f27687c16f61f4b032db5a0250ce87db
kid009 <pongpoom1985@gmail.com>
2019-10-01 16:21:36 +0700

    Add Project Workflow Webform

---
+++ WORKFLOW/WORKFLOW/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WORKFLOW.Startup))]
+namespace WORKFLOW
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
